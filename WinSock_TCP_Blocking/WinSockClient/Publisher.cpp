dc6e7a26c5d31b3bc6f9b25b65ac9b5f1ef8cd19
Moma1706 <57521686+Moma1706@users.noreply.github.com>
2020-12-15 17:35:00 +0100

    tcp comunication between server and client
    tcp comunication between server and client

---
+++ WinSock_TCP_Blocking/WinSockClient/Publisher.cpp
@@ -0,0 +1,105 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT 27016
+#define BUFER_SIZE 50
+
+typedef struct PUBLISHER_MESSAGE_ST {
+	char topic[50];
+	char message[50];
+} PUBLISHER_MESSAGE;
+
+// Initializes WinSock2 library
+// Returns true if succeeded, false otherwise.
+bool InitializeWindowsSockets();
+
+int  main() 
+{
+    // socket used to communicate with server
+    SOCKET connectSocket = INVALID_SOCKET;
+    
+	// struct uset to send topic and message
+	PUBLISHER_MESSAGE publisher_message;
+
+	// variable used to store function return value
+    int iResult;
+    // message to send
+    char *messageToSend = "this is a test";
+
+    if(InitializeWindowsSockets() == false)
+    {
+		// we won't log anything since it will be logged
+		// by InitializeWindowsSockets() function
+		return 1;
+    }
+
+    // create a socket
+    connectSocket = socket(AF_INET,
+                           SOCK_STREAM,
+                           IPPROTO_TCP);
+
+    if (connectSocket == INVALID_SOCKET)
+    {
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    // create and initialize address structure
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddress.sin_port = htons(DEFAULT_PORT);
+    // connect to server specified in serverAddress and socket connectSocket
+    if (connect(connectSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+    {
+        printf("Unable to connect to server.\n");
+        closesocket(connectSocket);
+        WSACleanup();
+    }
+	while (1) 
+	{
+		printf("Enter Topic:");
+		gets_s(publisher_message.topic,BUFER_SIZE);
+
+		printf("Enter Message:");
+		gets_s(publisher_message.message, BUFER_SIZE);
+
+		// Send an prepared message with null terminator included
+		iResult = send(connectSocket, (char*)&publisher_message, sizeof(publisher_message), 0);
+
+		if (iResult == SOCKET_ERROR)
+		{
+			printf("send failed with error: %d\n", WSAGetLastError());
+			closesocket(connectSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		printf("Bytes Sent: %ld\n", iResult);
+	}
+    // cleanup
+    closesocket(connectSocket);
+    WSACleanup();
+
+    return 0;
+}
+
+bool InitializeWindowsSockets()
+{
+    WSADATA wsaData;
+	// Initialize windows sockets library for this process
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
+    {
+        printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+        return false;
+    }
+	return true;
+}
---
+++ WinSock_TCP_Blocking/WinSockServer/Server.cpp
@@ -0,0 +1,230 @@
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27016"
+#define MAX_CLIENTS 100
+#define BUFER_SIZE 50
+
+typedef struct PUBLISHER_MESSAGE_ST {
+	char topic[BUFER_SIZE];
+	char message[BUFER_SIZE];
+} PUBLISHER_MESSAGE;
+
+bool InitializeWindowsSockets();
+
+int  main(void)
+{
+	// Socket used for listening for new clients 
+	SOCKET listenSocket = INVALID_SOCKET;
+	// Socket used for communication with client
+	SOCKET acceptedSocket[MAX_CLIENTS];
+	short lastIndex = 0;
+	// variable used to store function return value
+	int iResult;
+	// Buffer used for storing incoming data
+	char recvbuf[DEFAULT_BUFLEN];
+
+	fd_set readfds;
+	FD_ZERO(&readfds);
+
+	// maksimalni period cekanja select funkcije
+	timeval timeVal;
+	timeVal.tv_sec = 1;
+	timeVal.tv_usec = 0;
+
+
+	if (InitializeWindowsSockets() == false)
+	{
+		// we won't log anything since it will be logged
+		// by InitializeWindowsSockets() function
+		return 1;
+	}
+
+	// Prepare address information structures
+	addrinfo *resultingAddress = NULL;
+	addrinfo hints;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;       // IPv4 address
+	hints.ai_socktype = SOCK_STREAM; // Provide reliable data streaming
+	hints.ai_protocol = IPPROTO_TCP; // Use TCP protocol
+	hints.ai_flags = AI_PASSIVE;     // 
+
+	// Resolve the server address and port
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &resultingAddress);
+	if (iResult != 0)
+	{
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	// Create a SOCKET for connecting to server
+	listenSocket = socket(AF_INET,      // IPv4 address famly
+		SOCK_STREAM,  // stream socket
+		IPPROTO_TCP); // TCP
+
+	if (listenSocket == INVALID_SOCKET)
+	{
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(resultingAddress);
+		WSACleanup();
+		return 1;
+	}
+
+	unsigned long mode = 1;
+	iResult = ioctlsocket(listenSocket, FIONBIO, &mode);
+
+	// Setup the TCP listening socket - bind port number and local address 
+	// to socket
+	iResult = bind(listenSocket, resultingAddress->ai_addr, (int)resultingAddress->ai_addrlen);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(resultingAddress);
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// Since we don't need resultingAddress any more, free it
+	freeaddrinfo(resultingAddress);
+
+	// Set listenSocket in listening mode
+	iResult = listen(listenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("listen failed with error: %d\n", WSAGetLastError());
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	printf("Server initialized, waiting for clients.\n");
+	
+	while (1) {
+		FD_ZERO(&readfds);
+		if (lastIndex != MAX_CLIENTS)
+		{
+			FD_SET(listenSocket, &readfds);
+		}
+
+		for (int i = 0; i < lastIndex; i++) {
+			if (acceptedSocket[i] != INVALID_SOCKET)
+			{
+
+				FD_SET(acceptedSocket[i], &readfds);
+			}
+		}
+		int result = select(0, &readfds, NULL, NULL, &timeVal);
+
+		if (result == 0) {
+			printf("Nothing happened\n");
+		}
+		else if (result == SOCKET_ERROR) {
+
+			printf("listen failed with error: %d\n", WSAGetLastError());
+			closesocket(listenSocket);
+			WSACleanup();
+			return 1;
+
+		}
+		else if (FD_ISSET(listenSocket, &readfds))
+		{
+			acceptedSocket[lastIndex] = accept(listenSocket, NULL, NULL);
+
+			if (acceptedSocket[lastIndex] == INVALID_SOCKET)
+			{
+				printf("accept failed with error: %d\n", WSAGetLastError());
+				closesocket(listenSocket);
+				WSACleanup();
+				return 1;
+			}
+
+			unsigned long mode = 1;
+			iResult = ioctlsocket(acceptedSocket[lastIndex], FIONBIO, &mode);
+			lastIndex++;
+		}
+		else
+		{
+			for (int i = 0; i < lastIndex; i++)
+			{
+				if (FD_ISSET(acceptedSocket[i], &readfds))
+				{
+					// Receive data until the client shuts down the connection7
+					iResult = recv(acceptedSocket[i], recvbuf, DEFAULT_BUFLEN, 0);
+
+					if (iResult > 0)
+					{
+						if (iResult > 50) //message forom publisher
+						{
+							//recieve struct from publisher
+							PUBLISHER_MESSAGE publisher_message = *(PUBLISHER_MESSAGE*)recvbuf;
+							printf("Message received from Publisher: \nTopic: %s\nMessage: %s \n", publisher_message.topic, publisher_message.message);
+						}
+						else if (iResult <= 50)
+						{
+							printf("Message recieved from Subscriber: \nTopic: %s\n", recvbuf);
+						}
+					}
+					else if (iResult == 0)
+					{
+						// connection was closed gracefully
+						printf("Connection with client closed.\n");
+						closesocket(acceptedSocket[i]);
+						// sort array and clean last place
+						for (int j = i; j < lastIndex - 1; j++)
+						{
+							acceptedSocket[j] = acceptedSocket[j + 1];
+						}
+						acceptedSocket[lastIndex - 1] = 0;
+
+						lastIndex--;
+					}
+					else
+					{
+						// there was an error during recv
+						printf("recv failed with error: %d\n", WSAGetLastError());
+						closesocket(acceptedSocket[i]);
+						// sort array and clean last place
+						for (int j = i; j < lastIndex - 1; j++)
+						{
+							acceptedSocket[j] = acceptedSocket[j + 1];
+						}
+						acceptedSocket[lastIndex - 1] = 0;
+
+						lastIndex--;						
+					}
+
+				}
+				else
+				{
+					printf("No message recieved\n");
+					Sleep(2000);
+				}
+			}
+		}
+	}
+		
+	// cleanup
+	closesocket(listenSocket);
+	WSACleanup();
+
+	return 0;
+}
+
+bool InitializeWindowsSockets()
+{
+	WSADATA wsaData;
+	// Initialize windows sockets library for this process
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+		return false;
+	}
+	return true;
+}
+
+
